80d5a4a3ff6c4c6cc9f1480fc8627f5c6cc04ee6
Adam Gotlib <gotlib.adam+dev@gmail.com>
2018-09-25 03:53:43 +0200

    Add position offset publisher based on precomputed data

---
+++ selfie_map_processing/scripts/offset_publisher.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import rospy
+import tf2_ros
+import math
+import sys
+import pickle
+
+from scipy import interpolate
+from std_msgs.msg import Float64
+
+UPDATE_RATE = 50
+
+def generate_interpolation(offsets, resolution, origin):
+    x = []
+    y = []
+    z = []
+    for ix in range(offsets.shape[1]):
+        for iy in range(offsets.shape[0]):
+            x.append(ix*resolution + map_data['origin'][0])
+            y.append((offsets.shape[0] - iy - 1)*resolution + map_data['origin'][1])
+            z.append(offsets[iy, ix])
+
+    return interpolate.interp2d(x, y, z)
+
+if __name__ == '__main__':
+    rospy.init_node('selfie_controller')
+
+    # Read output of map preprocessing
+    filename = sys.argv[1]
+    with open(filename, 'rb') as f:
+        map_data = pickle.load(f)
+
+    eval_offset = generate_interpolation(map_data['offsets'],
+                                         map_data['resolution'],
+                                         map_data['origin'])
+
+    # Announce topic publisher
+    offset_pub = rospy.Publisher('position_offset', Float64, queue_size=UPDATE_RATE)
+
+    # Configure transform listener
+    tf_buffer = tf2_ros.Buffer()
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+
+    # Set update/publishing rate
+    rate = rospy.Rate(UPDATE_RATE)
+
+    while not rospy.is_shutdown():
+        current_time = rospy.Time.now()
+
+        try:
+            t = tf_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+
+        offset_pub.publish(Float64(eval_offset(t.transform.translation.x,
+                                               t.transform.translation.y)))
+
+        rate.sleep()
