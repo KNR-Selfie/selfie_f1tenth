67489706c3b9590ace288b256f4363bd39bfb749
nels3 <kornelialukojc@gmail.com>
2018-09-29 00:33:22 +0200

    Fixed segmentation fault

--- selfie_basic_steering/src/main.cpp
+++ selfie_basic_steering/src/main.cpp
@@ -9,30 +9,19 @@ SteerData data_to_send;
 
 void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& message);
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "basic_steering");
-  ros::NodeHandle node;
+ros::NodeHandle node;
   ros::Publisher steering_publisher = node.advertise<std_msgs::Float64>("steering_state", 100);
   ros::Subscriber lidar_subscriber = node.subscribe("scan", 1, lidarCallback);
   std_msgs::Float64 state_msg;
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "basic_steering");
+  
   
   while (ros::ok())
   {
-    // Process data
-    process.polar_to_cartesian();
-    process.simplify_data();
-    process.split_poins_equally();
-    process.search_points();
-    process.calc_mid();
-    process.calc_offsets();
-    process.calc_slopes();
-    
-    // Publish data
-    process.pack_data(data_to_send);
-    state_msg.data = data_to_send.offset[1]*0.8 + data_to_send.offset[0]*0.2;
-    steering_publisher.publish(state_msg);
-
+  
      // Next step
     ros::spinOnce();
   }
@@ -48,5 +37,19 @@ void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& message)
     for(uint32_t i = 0; i<message->ranges.size(); i++){
         process.raw_data.push_back (message->ranges[i]);
     }
+
+    // Process data
+    process.polar_to_cartesian();
+    process.simplify_data();
+    process.split_poins_equally();
+    process.search_points();
+    process.calc_mid();
+    process.calc_offsets();
+    process.calc_slopes();
+    
+    // Publish data
+    process.pack_data(data_to_send);
+    state_msg.data = data_to_send.offset[1]*0.8 + data_to_send.offset[0]*0.2;
+    steering_publisher.publish(state_msg);
 }
 
