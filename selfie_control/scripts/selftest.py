2d5f5dff15c71f9f3ab615f425a2672260e4e23b
AvaRat <marcelos.kalinsi@gmail.com>
2019-04-05 16:51:38 +0200

    check_x_acceleration

--- selfie_control/scripts/selftest.py
+++ selfie_control/scripts/selftest.py
@@ -9,7 +9,8 @@ from ackermann_msgs.msg import AckermannDriveStamped
 class tester:
   def __init__(self):
     self.set_speed = 0.5
-    self.wait_time = 1
+    self.set_acceleration = 5
+    self.wait_time = 0.5  #seconds
     self.imu_msgs = []
     self.dist_msgs = []
     self.speed_msgs = []
@@ -17,11 +18,13 @@ class tester:
     self.msg_stop = AckermannDriveStamped()
     msg = AckermannDriveStamped()
     msg.drive.speed = self.set_speed
-    msg.drive.acceleration = 5
+    msg.drive.acceleration = self.set_acceleration
     msg.drive.steering_angle = 0
     msg.drive.steering_angle_velocity = 10
     self.msg_start = msg
     self.msg_stop = msg
+    self.msg_turn = msg
+    self.msg_turn.drive.steering_angle = 1  #skręt w prawo
     self.msg_stop.drive.speed = 0
 
   def imu_callback(self, data):
@@ -45,8 +48,10 @@ class tester:
       avg_speed = sum / n_msgs
     except ZeroDivisionError:
       pass
-    linear_acc = np.zeros([500, 3])
-    angular_vel = np.zeros([500, 3])
+
+    size = len(self.imu_msgs)
+    linear_acc = np.zeros([size, 3])
+    angular_vel = np.zeros([size, 3])
     
     i=0
     for msg in self.imu_msgs:
@@ -65,17 +70,42 @@ class tester:
       angular_vel[i,:] = ang_vec
       i = i+1
 
-    f = open('/home/marcel/catkin_ws/src/selfie_f1tenth/selfie_control/scripts/linear', 'wb+')
-    np.save(f, linear_acc)
-    f.close()
+  #   f = open('/home/marcel/catkin_ws/src/selfie_f1tenth/selfie_control/scripts/selftest_try/linear', 'wb+')
+  #   np.save(f, linear_acc)
+  #   f.close()
+  
+  #   f = open('/home/marcel/catkin_ws/src/selfie_f1tenth/selfie_control/scripts/selftest_try/angular', 'wb+')
+  #   np.save(f, angular_vel)
+  #   f.close()
 
-    f = open('/home/marcel/catkin_ws/src/selfie_f1tenth/selfie_control/scripts/angular', 'wb+')
-    np.save(f, angular_vel)
-    f.close()
+    avg_x_acc = np.mean(linear_acc[:,0])
+    avg_y_acc = np.mean(linear_acc[:,1])
+    avg_z_acc = np.mean(linear_acc[:,2])
 
+    avg_x_rot = np.mean(angular_vel[:,0])
+    avg_y_rot = np.mean(angular_vel[:,1])
+    avg_z_rot = np.mean(angular_vel[:,2])
+
+    print('linear acceleration: ' + repr(np.mean(linear_acc, axis=0)))
+    print('angular speed: ' + repr(np.mean(angular_vel, axis=0)))
 
     arr = self.dist_msgs
     dist = arr[len(arr)-1].data - arr[0].data
+
+    # x acceleration
+    if (self.set_acceleration - avg_x_acc > 1 ):
+      rospy.logerr('x acceleration = ' + repr(avg_x_acc) + 
+        ' but ' + repr(self.set_acceleration) + ' expected')
+    else:
+      rospy.loginfo('x acceleration as expected ( ' +repr(avg_x_acc) +
+         ' ) vs ' + repr(self.set_acceleration))
+    # gravitation
+    if((avg_z_acc - 9.81) > 0.5):
+      rospy.logerr('gravitation = ' + repr(avg_z_acc) + ' but ' + repr(9.81) + ' expected')
+    else:
+      rospy.loginfo('gravitation ok =' + repr(avg_z_acc))
+
+    #general stats
     rospy.loginfo('test time: ' + repr(self.wait_time) + ' s')
     rospy.loginfo('avg speed = '+ repr(avg_speed) + ' kmph')
     rospy.loginfo('distance travelled = ' + repr(dist) + ' m')
